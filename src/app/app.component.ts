09194cd2f6bf7066ab8e67f78767f5806844c86d
Vijay Yadav <vij8922@gmail.com>
2024-01-10 20:09:26 +0530

    create component product-list and initialized products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventory-app';
+  products: Product[];
+  constructor(){
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '',
+        ['Men','Shoes','Runnig Shoes'],
+        45.99
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '',
+        ['Women','Apparel','Jacket & Vests'],
+        55.99
+      ),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '',
+        ['Men','Accessories','Hats'],
+        38.39
+      ),
+  ]}
+  productWasSelected(product : Product): void{
+    console.log('Product Clicked: ',product)
+  }
 }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  @Input()
+  product!: Product;
+}
