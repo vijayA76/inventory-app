d2dac36c465d99dec686c968127cd2f9e6a9d3b7
Vijay Yadav <vij8922@gmail.com>
2024-01-11 15:35:59 +0530

    add clicked and isSelected method

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,7 +12,19 @@ export class ProductListComponent {
   @Output()
   onProductSelected !: EventEmitter<Product>;
 
+  private currentProduct !: Product;
+
   constructor(){
     this.onProductSelected = new EventEmitter();
+  }
+  clicked(product : Product): void{
+    this.currentProduct = product;
+    this.onProductSelected.emit(this.currentProduct)
+  }
+  isSelected(product : Product):boolean{
+    if(!product || !this.currentProduct){
+      return false;
+    }
+    return product.sku==this.currentProduct.sku
   }
 }
